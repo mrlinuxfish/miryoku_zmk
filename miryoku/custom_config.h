791599fd4e12ed1b12dfce163745355439235f44
Violet Bosveld <steamyleafjuice@protonmail.com>
2024-12-30 15:55:04 -0700

    Update custom_config.h
    Add custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
-// Copyright 2021 Manna Harbour
+// Copyright 2024 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+// #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
+#define MIRYOKU_LAYER_TAP \
+&kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp L,             &kp U,             &kp Y,             &to U_BASE,        \
+&kp LSHIFT,        &kp A,             &kp S,             &kp D,             &kp F,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
+&kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp SPC,           &kp TAB,           &kp ESC,           &kp LS(TAB),       &kp DEL,           U_NP,              U_NP
